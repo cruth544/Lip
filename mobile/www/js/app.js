243dc2e7ece179d5a479f97a6a69dfa22689dff5
Chad Rutherford <c.ruth544@gmail.com>
2016-02-14 15:49:15 -0800

    song view works

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -60,7 +60,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       }
     })
     .state('tab.song-view', {
-      url: '/songs/:songId',
+      url: '/song/:songId',
       views: {
         'tab-songs': {
           templateUrl: 'templates/song-view.html',
